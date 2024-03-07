c056ef207ccf75c19698484934dffa72c95f5cc0
Jagmeister <unsaid-09.imagers@icloud.com>
2024-03-07 15:41:53 -0500

    Initial Commit

---
+++ PalworldTools/PalworldToolsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PalworldToolsApp.swift
+//  PalworldTools
+//
+//  Created by Javier Rendon on 3/7/24.
+//
+
+import SwiftUI
+
+@main
+struct PalworldToolsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
