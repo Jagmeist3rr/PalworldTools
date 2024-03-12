bf77400d205992dc41ccb430f6ed92fd0112f1e5
Jagmeister <Javier.ren@outlook.com>
2024-03-11 22:53:10 -0400

    Will be going from popup for items to a view for items so that it can be pulled easier when viewing mats in weapons, pals, etc

--- PalworldTools/Views/MatViews.swift
+++ PalworldTools/Views/MatViews.swift
@@ -8,24 +8,69 @@
 import Foundation
 import SwiftUI
 
-struct ItemsView: View {
+struct MatsView: View {
     @Environment(\.presentationMode) var presentationMode
     @EnvironmentObject var materialsListManager: MaterialsListManager
+    @EnvironmentObject var materialsManager: MaterialsManager
+    var item: materialsList
     
     var body: some View {
-            VStack {
-                Text("Test")
-                NavigationLink("Materials", destination: MaterialsView())
-                
-            }
-            .navigationTitle("Items")
-        }
+        let roundedweight = String(format: "%.2f", item.fullPage.weight)
         
+        VStack (){
+            HStack() { // Align items to the top
+                item.image
+                Text(item.name)
+            }
+            Spacer()
+                VStack {
+                    HStack{
+                        Text("Weight:")
+                            .padding(.leading)
+                        Text("\(roundedweight)")
+                        Spacer()
+
+                    }
+                    HStack{
+                        Text("Buy Price:")
+                            .padding(.leading)
+                        Text("\(item.fullPage.buyPrice)")
+                        Spacer()
+
+                    }
+                    HStack{
+                        Text("Sell Price:")
+                            .padding(.leading)
+                        Text("\(item.fullPage.sellPrice)")
+                        Spacer()
+
+                    }
+                }
+            
+                Spacer()
+                VStack {
+                    HStack{
+                        Text("How to Obtain:")
+                            .padding(.leading)
+                        Spacer()
+                    }
+            }
+            Spacer()
+            Spacer()
+            Spacer()
+            Spacer()
+            Spacer()
+            Spacer()
+            Spacer()
+            Spacer()
+            Spacer()
+        }
+    }
 }
 
 struct ItemsView_Previews: PreviewProvider {
     static var previews: some View {
-        ItemsView()
+        MatsView(item: materialsList(name: "Gold Coin", image: Image("Gold_Coin_icon"), fullPage: materialsStruct(name: "Gold Coin", icon: Image("Gold_Coin_icon"), weight: 0, buyPrice: 0, sellPrice: 0, sources: ["Pals", "Trading"])))
             .environmentObject(MaterialsListManager())
     }
 }
