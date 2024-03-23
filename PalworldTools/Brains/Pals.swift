50272e0d5ae0ef628d53dcb3a768ac08ec1efec8
Jagmeister <Javier.ren@outlook.com>
2024-03-23 10:19:29 -0400

    added pals up to 15 not counting subspieces and their images and drops

--- PalworldTools/Brains/Pals.swift
+++ PalworldTools/Brains/Pals.swift
@@ -9,7 +9,6 @@ import Foundation
 import SwiftUI
 
 struct Pals: Hashable {
-    
     var name: String
     var icon: Image
     var title: String
@@ -48,12 +47,6 @@ extension Pals: Equatable {
     }
 }
 
-struct workSuitability{
-    var name: String
-    var icon: Image
-    var level: Int
-}
-
 class PalsWrapper: ObservableObject {
     @Published var pals: Pals
     
@@ -63,6 +56,7 @@ class PalsWrapper: ObservableObject {
 }
 
 class PalsManager: ObservableObject{
+    //Class where Pal information is good
     @Published var palsManager: [Pals]
     init(palsManager: [Pals]) {
         self.palsManager = palsManager
@@ -72,7 +66,6 @@ class PalsManager: ObservableObject{
             foodImages.append(Image("Food_On")) // Replace "Food_On" with your actual food image name
         }
         
-
         self.palsManager = [
             Pals(name: "Lamball", icon: Image("Lamball"), title: "Big Floof", number: "#001", element: ["Neutral"], drops: ["Wool", "Lamball Mutton"], food: 2, foodimage: foodImages, partnerSkill: "Fluffy Shield", partnerSkillDescription: "When activated, equips to the player and becomes a shield. Sometimes drops Wool when assigned to Ranch.", worksuitabilty: ["Handiwork": 1, "Transporting": 1, "Farming": 1]),
             Pals(name: "Cattiva", icon: Image("Cattiva"), title: "The Cat's Pajamas", number: "#002", element: ["Neutral"], drops: ["Red Berries"], food: 2, foodimage: foodImages, partnerSkill: "Cat Helper", partnerSkillDescription: "While in team, Cattiva helps carry supplies, increasing the player's max carrying capacity.", worksuitabilty: ["Handiwork": 1,"Gathering":1,"Mining":1,"Transporting":1]),
@@ -81,11 +74,19 @@ class PalsManager: ObservableObject{
             Pals(name: "Foxparks", icon: Image("Foxparks"), title: "Revealer of Paths", number: "#005", element: ["Fire"], drops: ["Leather", "Flame Organ"], food: 2, foodimage: foodImages, partnerSkill: "Huggy Fire", partnerSkillDescription: "When activated, leaps into the player's head and uses a submachine gun to follows up player attacks.", worksuitabilty: ["Kindling": 1]),
             Pals(name: "Fuack", icon: Image("Fuack"), title: "Rowdy Wave Ripper", number: "#006", element: ["Water"], drops: ["Leather", "Pal Fluids"], food: 2, foodimage: foodImages, partnerSkill: "Surfing Slam", partnerSkillDescription: "When activated, Fuack body surfs towards an enemy and slams into them.", worksuitabilty: ["Watering": 1, "Handiwork": 1, "Transporting": 1]),
             Pals(name: "Sparkit", icon: Image("Sparkit"), title: "Livewire Tabby", number: "#007", element: ["Electric"], drops: ["Electric Organ"], food: 2, foodimage: foodImages, partnerSkill: "Static Electricity", partnerSkillDescription: "While in team, increases attack power of Electric Pals", worksuitabilty: ["Generating Electricity": 1, "Handiwork": 1, "Transporting": 1]),
-            Pals(name: "Tanzee", icon: Image("Tanzee"), title: "Dexterous Primate", number: "008#", element: ["Grass"], drops: ["Mushroom"], food: 2, foodimage: foodImages, partnerSkill: "Cheery Rifle", partnerSkillDescription: "When activated, Tanzee will mercilessly fire an assault rifle at nearby enemies.", worksuitabilty: ["Plating": 1, "Handiwork": 1, "Gathering": 1, "Transporting": 1])
+            Pals(name: "Tanzee", icon: Image("Tanzee"), title: "Dexterous Primate", number: "008#", element: ["Grass"], drops: ["Mushroom"], food: 2, foodimage: foodImages, partnerSkill: "Cheery Rifle", partnerSkillDescription: "When activated, Tanzee will mercilessly fire an assault rifle at nearby enemies.", worksuitabilty: ["Plating": 1, "Handiwork": 1, "Gathering": 1, "Transporting": 1]),
+            Pals(name: "Rooby", icon: Image("Rooby"), title: "Chief of the Herd", number: "#009", element: ["Fire"], drops: ["Flame Organ", "Leather"], food: 2, foodimage: foodImages, partnerSkill: "Tiny Spark", partnerSkillDescription: "While in team, increases attach power of Fire Pals", worksuitabilty: ["Kindling": 1]),
+            Pals(name: "Pengullet", icon: Image("Pengullet"), title: "Ate Too Much", number: "#010", element: ["Water", "Ice"], drops: ["Ice Organ", "Pal Fluids"], food: 2, foodimage: foodImages, partnerSkill: "Pengullet Cannon", partnerSkillDescription: "When activated, the player equips a Rocket Launcher and fires Pengullet as ammunition. Pengullet explodes on contact and is incapacitated.", worksuitabilty: ["Watering": 1, "Handiwork": 1, "Cooling": 1, "Transporting": 1]),
+            Pals(name: "Penking", icon: Image("Penking"), title: "Pioneer of the Frozen Sea", number: "#011", element: ["Water", "Ice"], drops: ["Ice Organ", "Penking Plume"], food: 8, foodimage: foodImages, partnerSkill: "Brave Sailor", partnerSkillDescription: "When fighting together, Fire Pals drop more items when defeated", worksuitabilty: ["Water": 2, "Handiwork": 2, "Mining": 2, "Cooling": 2, "Transporting": 2]),
+            Pals(name: "Jolthog", icon: Image("Jolthog"), title: "Don't Touch!", number: "012", element: ["Electric"], drops: ["Electric Orga"], food: 2, foodimage: foodImages, partnerSkill: "Jolt Bomb", partnerSkillDescription: "When activated, equips Jolthog to the player's hand. When thrown at an enemy, causes an electrical explosion upon impact.", worksuitabilty: ["Generating Electricity": 1]),
+            Pals(name: "Gumoss", icon: Image("Gumoss"), title: "Suddenly Transformed", number: "#013", element: ["Grass", "Ground"], drops: ["Berry Seeds", "Gumoss Leaf"], food: 1, foodimage: foodImages, partnerSkill: "Logging Assistance", partnerSkillDescription: "While in team, improves efficiency of cuttinh trees", worksuitabilty: ["Planting": 1]),
+            Pals(name: "Vixy", icon: Image("Vixy"), title: "Star of the Steppe", number: "#014", element: ["Neutral"], drops: ["Leather", "Bones"], food: 1, foodimage: foodImages, partnerSkill: "Dig Here!", partnerSkillDescription: "Sometimes digs up items from the ground when assigned to Ranch", worksuitabilty: ["Gathering": 1, "Farming": 1]),
+            Pals(name: "Hoocrates", icon: Image("Hoocrates"), title: "Purveyor of Wisdom", number: "#015", element: ["Dark"], drops: ["Fiber", "High Grade Technical Manual"], food: 3, foodimage: foodImages, partnerSkill: "Dark Knowledge", partnerSkillDescription: "While in team, increases attack of Dark Pals", worksuitabilty: ["Gathering": 1])
         ]
     }
     func elementIcons(element: String) -> Image{
         switch element{
+            //Displays icon for images
         case "Neutral":
             return Image("\(element)_icon")
         case "Dark":
@@ -111,6 +112,7 @@ class PalsManager: ObservableObject{
     
     func workIcons(work: String) -> Image{
         switch work{
+            //Displays Icons for work suitabilty
         case "Handiwork":
             return Image("\(work)_Icon")
         case "Kindling":
@@ -142,15 +144,15 @@ class PalsManager: ObservableObject{
     
     func colorForElement(workname: String, worklist: [String: Int]) -> Color {
         if worklist.keys.contains(workname) {
-            // Return a specific color when the element is found in the array
-            return .black // Change this to the desired color
+            // Used to change font to black if work suitability is found for pal
+            return .black
         } else {
-            // Return a default color when the element is not found in the array
-            return .white // Change this to the desired color
+            return .white
         }
     }
     
     func numberForElement(workname: String, worklist: [String: Int]) -> Int {
+        //returns value of work suitabilty if found for pal
         if let value = worklist[workname] {
             return value
         } else {
